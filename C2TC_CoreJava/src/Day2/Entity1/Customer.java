04a15b961cc2fbd410baab76364fc33a0e3f42dd
HP <HP@DESKTOP-LAIQ59B>
2025-07-23 15:06:46 +0530

    training

---
+++ C2TC_CoreJava/src/Day2/Entity1/Customer.java
@@ -0,0 +1,45 @@
+package Day2.Entity1;
+
+public class Customer {
+	 private int cid;
+	 private String cname;
+	 private String city;
+	  
+	 
+	 public Customer() {
+		 this.cid =100;
+		 this.cname="Naruto";
+		 this.city ="hidden leef";
+	 }
+	 public Customer(int cid,String cname,String city) {
+		 this.cid =cid;
+		 this.cname=cname;
+		 this.city =city;
+	 }
+	 public int getCid() {
+		 return cid;
+	 }
+	 public void setCid(int cid) {
+		 this.cid = cid;
+	 }
+	 public String getCname() {
+		 return cname;
+	 }
+	 public void setCname(String cname) {
+		 this.cname = cname;
+	 }
+	 public String getCity() {
+		 return city;
+	 }
+	 public void setCity(String city) {
+		 this.city = city;
+	 }
+	 @Override
+	 public String toString() {
+		return "Customer [cid=" + cid + ", cname=" + cname + ", city=" + city + "]";
+	 }
+	 
+	 
+	 
+	 
+}
---
+++ C2TC_CoreJava/src/Day2/Entity1/CustomerDemo.java
@@ -0,0 +1,21 @@
+package Day2.Entity1;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+																																														
+		Customer ob = new Customer();
+		Customer ob1 = new Customer();
+				
+		ob.setCid(101);
+		ob.setCity("chennai");
+		ob.setCname("Shakthi");
+
+		ob1.setCid(102);
+		ob1.setCity("chennai");
+		ob1.setCname("siva");
+		System.out.println(ob1);
+		System.out.println("Customer 1 city : "+ob.getCity()+" Customer 2 city : "+ob1.getCity());
+	}
+
+}
